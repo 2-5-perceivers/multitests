ba0da1e14069234b219d82d447cc48e7452355b6
Rares Vanca <raresvanca@gmail.com>
2022-08-27 20:35:04 +0300

    Open-sourcing

--- lib/tests/tests.dart
+++ lib/tests/tests.dart
@@ -1,1 +1,10 @@
 export 'purity_test.dart';
+
+/* How to create a test:
+- Create a new file with yor test name
+- Inside create a test using the Test class
+- export the file here
+- add the test to the registry in the main function in main.dart
+
+or:
+- ask for help with the request test request issue template */
